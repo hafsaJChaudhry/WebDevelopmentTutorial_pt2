8b8c19893d318e4857d444b82e231d16296332ff
hafsaJChaudhry <60797103+hafsaJChaudhry@users.noreply.github.com>
2024-01-11 23:56:04 -0500

    added files

--- app.py
+++ app.py
@@ -1,42 +1,83 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'NYC, New York',
-    'salary': '$200,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Tysons, Virginia',
-    'salary': '$150,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-  }
-]
-
 @app.route("/")
 def hello_hafsa():
-    return render_template('home.html', 
-                           jobs=JOBS, 
-                           company_name='Hafsa')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', 
+                           jobs=jobs)
 
+#@app.route("/api/jobs")
+#@app.route('/jobs/<int:job_id>', methods=['GET'])
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<job_id>")
+def show_job(job_id):
+    job = load_job_from_db(job_id)
+
+    if not job:
+        return "Job not found", 404
+
+    return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>")
+def show_job_api(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+#@app.route("/job/<int:id>/apply", methods=['GET', 'POST'])
+@app.route("/job/<int:id>/apply", methods=['post'])
+def apply_to_job(id):
+  #get data from the form
+  data = request.form
+  #get job id (for HTML so we can print out job title)
+  job = load_job_from_db(id)
+
+  #add data to database
+  add_application_to_db(id, data)
+  
+  return render_template('application_submitted.html', application=data, role=job)
+  '''
+  if request.method == 'GET':
+    # Handle GET request (if needed)
+    job = load_job_from_db(id)
+    if job:
+      print("Rendering apply_form.html for GET request")
+      return render_template('apply_form.html', job=job)
+    else:
+      print("Job not found")
+      return "Job not found", 404
+  else:
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    print("Application submitted successfully")
+    return render_template('application_submitted.html', application=data, job=job)
+  '''
+  '''
+  if request.method == 'POST':
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    print("Application submitted successfully")
+    return render_template('application_submitted.html', application=data, job=job)
+  else:
+     # Handle GET request (if needed)
+    job = load_job_from_db(id)
+    if job:
+      print("Rendering apply_form.html for GET request")
+      return render_template('apply_form.html', job=job)
+    else:
+      print("Job not found")
+      return "Job not found", 404
+    '''
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine, text
+from flask import request
+import os
+
+db_connection = "mysql+pymysql://dpdjqe2u2whcnfx93ltx:pscale_pw_tuQpyby6RG3kxnJPfacmj2rMf02SyO0fOlDdJ4MOZcW@aws.connect.psdb.cloud/hafsacareers?charset=utf8mb4"
+
+#connect to our mysql database
+engine = create_engine(db_connection,
+                      connect_args={
+                        "ssl": {
+                          "ssl_ca": "/etc/ssl/cert.pem"
+                        }
+                      })
+
+with engine.connect() as conn: 
+  result = conn.execute(text("SELECT * FROM jobs"))
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+
+    # Fetch the column names from the result set
+    columns = result.keys()
+
+    # Create a list of dictionaries from the result set
+    jobs = [dict(zip(columns, row)) for row in result]
+    
+    return jobs
+
+def load_job_from_db(job_id):
+  with engine.connect() as conn:
+    # Get job with ID from the jobs table using parameter binding
+    query = text("SELECT * FROM jobs WHERE id = :job_id")
+    result = conn.execute(query, {'job_id': job_id})
+
+    row = result.fetchone()
+    # If the job with the specified ID exists, return it as a dictionary
+    if row:
+        columns = result.keys()
+        job_dict = dict(zip(columns, row))
+        return job_dict
+
+  return None
+
+#Adding data into our table 
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    #have form request variables
+    query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')")
+
+    conn.execute(query)
+
+
+  
